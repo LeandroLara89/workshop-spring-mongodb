aae8c64c9c09bdd6e72fdfe4dcb5f35b00616087
Leandro Lara <leandroshinigami@gmail.com>
2019-08-22 15:10:13 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/leandrolara/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leandrolara/workshopmongo/resources/UserResource.java
@@ -48,5 +48,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	//@GatMapping - pode usar essa escolha tb.
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
